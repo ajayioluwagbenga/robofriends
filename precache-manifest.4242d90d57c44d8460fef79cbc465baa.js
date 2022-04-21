eaba38fa06943b5baf9d7b4d23f7bfff4e009c08
ajayioluwagbenga <ajayioluwagbenga1993@gmail.com>
2022-04-21 17:12:23 +0100

    Updates

---
+++ precache-manifest.4242d90d57c44d8460fef79cbc465baa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cebbe3d29674e3b8ff6a",
+    "url": "/robofriends/static/css/main.1a09d80a.chunk.css"
+  },
+  {
+    "revision": "cebbe3d29674e3b8ff6a",
+    "url": "/robofriends/static/js/main.cebbe3d2.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/robofriends/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "0ca65d53b7adede08184",
+    "url": "/robofriends/static/css/2.0b58b51c.chunk.css"
+  },
+  {
+    "revision": "0ca65d53b7adede08184",
+    "url": "/robofriends/static/js/2.0ca65d53.chunk.js"
+  },
+  {
+    "revision": "db08b936970c55357179",
+    "url": "/robofriends/static/js/runtime~main.db08b936.js"
+  },
+  {
+    "revision": "9fc85520791e54444711380e199e03af",
+    "url": "/robofriends/index.html"
+  }
+];
